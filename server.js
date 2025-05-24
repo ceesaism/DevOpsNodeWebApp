4bfd007b1e29b158b08f02d0e5f4559ef8b32eb1
ceesaism <ceesaism@students.zhaw.ch>
2025-05-24 12:39:14 +0200

    GitHub-Actions

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('(-: Hello ceesaism, Lernjournal 10');
+  res.send('(-: Hello ceesaism, Lernjournal 10, with GitHub Actions');
 });
 
 app.listen(PORT, HOST);
